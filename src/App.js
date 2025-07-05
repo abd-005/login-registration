36cdb6dd5dbc4287eed60b3a06e96446cedf29e0
MD. ABDUL AHAD <abd.me7@gmail.com>
2025-07-05 22:06:39 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginRegister from './Components/Assets/LoginRegiser/LoginRegister';
+
+function App() {
+  return (
+    <div>
+     <LoginRegister/>
+    </div>
+  );
+}
+
+export default App;
